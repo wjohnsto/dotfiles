c510c8689a5588964025a121b4775617db8c9dc3
Will Johnston <will-github@wwj.dev>
2024-06-02 10:37:55 -0700

    removing initial config

--- nvim/after/plugin/telescope.lua
+++
@@ -1,9 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > " ) });
-end)
-vim.keymap.set('n', '<leader>o', builtin.oldfiles, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-vim.keymap.set('n', '<leader>q', builtin.quickfix, {})
--- nvim/lua/wjohnsto/init.lua
+++ nvim/lua/wjohnsto/init.lua
@@ -1,2 +1,20 @@
 require("wjohnsto.remap")
 require("wjohnsto.set")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
