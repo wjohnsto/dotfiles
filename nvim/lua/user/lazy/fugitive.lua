1db31d8463f82b149a1f2d245fb835863f9bf2b9
Will Johnston <will-github@wwj.dev>
2024-06-29 08:34:39 -0700

    more desc

--- nvim/lua/user/lazy/fugitive.lua
+++ nvim/lua/user/lazy/fugitive.lua
@@ -38,7 +38,7 @@ return {
       end,
     })
 
-    vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-    vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+    vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>", { desc = "diffget target" })
+    vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>", { desc = "diffget merge" })
   end,
 }
