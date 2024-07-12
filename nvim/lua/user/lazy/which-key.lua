8f9e3169d2c3252c66a0403b59dabde82501683e
Will Johnston <will-github@wwj.dev>
2024-07-12 16:31:05 -0700

    chore(which-key): @3

--- nvim/lua/user/lazy/which-key.lua
+++ nvim/lua/user/lazy/which-key.lua
@@ -1,18 +1,23 @@
 return {
-	-- WhichKey is a lua plugin for Neovim 0.5 that displays a popup
-	-- with possible key bindings of the command you started typing.
-	-- Heavily inspired by the original emacs-which-key and vim-which-key.
-	--
-	-- Amazing for beginners, but make sure you document your keybindings!
-	-- https://github.com/folke/which-key.nvim
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-	},
+  -- WhichKey is a lua plugin for Neovim 0.5 that displays a popup
+  -- with possible key bindings of the command you started typing.
+  -- Heavily inspired by the original emacs-which-key and vim-which-key.
+  --
+  -- Amazing for beginners, but make sure you document your keybindings!
+  -- https://github.com/folke/which-key.nvim
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
 }
