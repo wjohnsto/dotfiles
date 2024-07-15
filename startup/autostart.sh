50d8f5efce747ff452e543d62503d749e21fe1d8
Will Johnston <will-github@wwj.dev>
2024-07-15 08:24:36 -0700

    -dd

--- startup/autostart.sh
+++ startup/autostart.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 # Changes to System Utils workspace, then back again
 
-sleep 3
-
 wmctrl -s 1
-
-sleep 1
-
-/opt/docker-desktop/bin/docker-desktop
 
 sleep 2
 
 # sudo python3 ~/dns-changer/main.py & disown
 dns_changer & disown
 
-sleep 2
+sleep 1
 
 studio-controls &>/dev/null & disown
 
@@ -22,6 +16,3 @@ sleep 6
 
 carla ~/Carla/default.carxp &>/dev/null & disown
 
-sleep 2
-
-wmctrl -s 0
