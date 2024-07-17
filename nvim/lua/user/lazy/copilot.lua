715c6617c86348932a36bf875d01279fbb374068
Will Johnston <will-github@wwj.dev>
2024-07-17 08:40:19 -0700

    fix(font): using mono font

---
+++ nvim/lua/user/lazy/copilot.lua
@@ -0,0 +1,5 @@
+return {
+   -- copilot
+   -- https://github.com/github/copilot.vim
+  "github/copilot.vim"
+}
--- wezterm/fonts.lua
+++ wezterm/fonts.lua
@@ -4,8 +4,8 @@ local exports = {}
 function exports:apply(config)
   config.font_dirs = { "~/.fonts" }
   config.font = wezterm.font_with_fallback({
-    { family = "FiraMono Nerd Font", weight = "Medium" },
-    { family = "icomoon",            weight = "Medium" },
+    { family = "FiraMono Nerd Font Mono", weight = "Medium" },
+    { family = "icomoon",                 weight = "Medium" },
   })
 end
 
