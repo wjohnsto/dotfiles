9a140972c30025819bbfd15274f82c926c635575
Will Johnston <will-github@wwj.dev>
2024-07-17 08:41:28 -0700

    fix(font): regular

--- wezterm/fonts.lua
+++ wezterm/fonts.lua
@@ -4,7 +4,7 @@ local exports = {}
 function exports:apply(config)
   config.font_dirs = { "~/.fonts" }
   config.font = wezterm.font_with_fallback({
-    { family = "FiraMono Nerd Font Mono", weight = "Medium" },
+    { family = "FiraMono Nerd Font Mono", weight = "Regular" },
     { family = "icomoon",                 weight = "Medium" },
   })
 end
