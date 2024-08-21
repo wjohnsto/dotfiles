4328cc68ddfb940d2aa18f39bcdc820ca39ec294
Will Johnston <will-github@wwj.dev>
2024-08-21 11:33:28 -0700

    fix: autostart

--- startup/autostart.sh
+++ startup/autostart.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Changes to System Utils workspace, then back again
 
+sleep 2
+
 wmctrl -s 1
 
-sleep 2
+sleep 1
 
 # sudo python3 ~/dns-changer/main.py & disown
 dns_changer & disown
 
-sleep 1
+sleep 3
 
 studio-controls &>/dev/null & disown
 
